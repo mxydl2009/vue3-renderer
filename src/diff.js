00dc510acef69f992c5258e47dc8b40284c44aee
maxingyuan <mxydl2009@163.com>
2024-07-03 18:49:25 +0800

    完善diff算法

--- src/diff.js
+++ src/diff.js
@@ -45,8 +45,13 @@ export function diffWithoutKey(n1, n2, container, renderOptions) {
  */
 export function singleEndDiffWithKey(n1, n2, container, renderOptions) {
 	const { insert } = renderOptions;
-	const oldChildren = n1.children;
-	const newChildren = n2.children;
+	// 对children的元素进行过滤
+	const oldChildren = n1.children.filter(
+		(c) => typeof c === 'string' || (typeof c === 'object' && c !== null)
+	);
+	const newChildren = n2.children.filter(
+		(c) => typeof c === 'string' || (typeof c === 'object' && c !== null)
+	);
 	const oldLen = oldChildren.length;
 	const newLen = newChildren.length;
 	// 从前向后遍历新节点，记录新节点在旧节点列表中的索引里的最大值，在该新节点之后的节点如果索引都小于该最大值，表明这些节点都需要移动到该新节点的前面
@@ -61,7 +66,8 @@ export function singleEndDiffWithKey(n1, n2, container, renderOptions) {
 		// 遍历旧节点，试图找到新旧节点里可复用的节点
 		for (let j = 0; j < oldLen; j++) {
 			const oldNode = oldChildren[j];
-			if (isReusable(oldNode, newNode)) {
+			const canReuse = isReusable(oldNode, newNode);
+			if (canReuse) {
 				findReusedNode = true;
 				// 找到新旧节点中可复用的节点
 				patch(oldNode, newNode, container, null, renderOptions); // 移动前先进行打补丁操作，因为可能节点的props或者子节点有更新
@@ -76,6 +82,9 @@ export function singleEndDiffWithKey(n1, n2, container, renderOptions) {
 						// TODO: anchorNode一定会有el属性？按理说新节点上应该没有el属性吧
 						const anchor = anchorNode.el.nextSibling;
 						insert(newNode.el, container, anchor);
+					} else {
+						// 说明i === 0，第一个子节点，虽然不用动，但是需要将旧节点上的el属性拷贝过来
+						newNode.el = oldNode.el;
 					}
 				} else {
 					lastIndex = j;
@@ -87,10 +96,13 @@ export function singleEndDiffWithKey(n1, n2, container, renderOptions) {
 		// 没有找到可复用节点，说明新节点不在旧节点中，需要进行挂载
 		if (!findReusedNode) {
 			let anchor;
+			// 因为新节点是按次序处理的，所以第一个新节点处理完后肯定有el属性，那么后面的依次取前面一个节点的el属性是肯定没问题
 			if (i === 0) {
 				anchor = container.firstChild;
 			} else {
+				// TODO: 新节点都不知道自己需要挂载到哪，那么新节点应该都没有el属性
 				anchor = newChildren[i - 1].el && newChildren[i - 1].el.nextSibling;
+				console.log('挂载新节点的anchor是', anchor, newChildren[i - 1]);
 			}
 			patch(null, newChildren[i], container, anchor, renderOptions);
 		}
@@ -117,10 +129,14 @@ export function singleEndDiffWithKey(n1, n2, container, renderOptions) {
 function isReusable(oldNode, newNode) {
 	const oldNodeKey = oldNode.key;
 	const newNodeKey = newNode.key;
-	return (
-		newNodeKey &&
-		oldNodeKey &&
-		newNodeKey === oldNodeKey &&
-		newNode.type === oldNode.type
-	);
+	if (oldNode.type.name === 'keep-alive' && newNode.type.name === 'keep-alive')
+		return true;
+	if (
+		oldNodeKey === undefined ||
+		newNodeKey === undefined ||
+		oldNodeKey === null ||
+		newNodeKey === null
+	)
+		return false;
+	return newNodeKey === oldNodeKey && newNode.type === oldNode.type;
 }
